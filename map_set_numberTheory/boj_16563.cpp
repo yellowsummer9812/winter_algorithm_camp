d017c664e106e67d6bc5d6d3a2cdf1979903070e
yellowsummer <cshee9812@gmail.com>
2021-02-27 19:05:58 +0900

    에라토스테네스의 체 + 소인수 분해 정답코드

--- map_set_numberTheory/boj_16563.cpp
+++ map_set_numberTheory/boj_16563.cpp
@@ -1,40 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define MAX 1000001
 typedef long long ll;
 using namespace std;
 
-ll n;
-ll num[5000001]; // 0이면 소수, 1이면 소수 아님
-ll a[MAX];
-vector<ll> v;
+int n;
+int primeNums[500];
+bool isNotPrime[50000001];
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
-    for(ll i = 2; i * i <= 5000000; i++){
-        if(num[i] == 0){
-            for(ll j = i; i * j <= 5000000; j++){
-                    num[i * j] = 1;
-            } 
-            v.push_back(i); // 소수인 거 v에 담기
+
+    int j = 0;
+    isNotPrime[0] = isNotPrime[1] = true;
+    for(int i = 2; i * i <= 5000000; i++){
+        if(isNotPrime[i] == false){
+            primeNums[j++] = i;
+            for(int k = i * i; k <= 5000000; k += i){
+                isNotPrime[k] = true;
+            }
         }
     }
 
     cin>> n;
 
-    ll tmp;
-
-    for(ll i = 1; i <= n; i++){ 
+    int tmp;
+    while(n--){
         cin>> tmp;
-        for(ll j = 0; (j < v.size()) && (v[j] * v[j] <= tmp); j++){
-            while(tmp % v[j] == 0){
-                cout<<v[j]<< " ";
-                tmp /= v[j];
+
+        while(tmp > 1){
+            if(isNotPrime[tmp]){
+                for(int i = 0; i < j; i++){
+                    if(tmp % primeNums[i] == 0){
+                        cout<< primeNums[i]<< ' ';
+                        tmp /= primeNums[i];
+                        break;
+                    }
+                }
+            }else{
+                cout<< tmp;
+                tmp = -1;
             }
         }
-        if(tmp != 1)
-            cout<< tmp;
-        cout<< "\n";
+        cout<< '\n';
     }
 }
