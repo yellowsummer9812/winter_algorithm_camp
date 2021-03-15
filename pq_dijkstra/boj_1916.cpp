ba880e07f8b95332515622b82f7fcbc4d13fc22e
yellowsummer <cshee9812@gmail.com>
2021-03-15 10:45:08 +0900

    1916번 재채점 - 메모리초과 해결

--- pq_dijkstra/boj_1916.cpp
+++ pq_dijkstra/boj_1916.cpp
@@ -3,12 +3,15 @@
 #include <queue>
 #include <algorithm>
 #define INF 1e9
+typedef long long ll;
 using namespace std;
 
 int n, m;
 vector<pair<int, int>> bus[1001];
 priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
 int dist[1001];
+bool visit[1001];
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
@@ -30,9 +33,10 @@ int main(){
     while(!pq.empty()){
         auto cur = pq.top(); pq.pop();
 
-        if(dist[cur.second] < cur.first) continue;
+        if(visit[cur.second]) continue;
+        visit[cur.second] = true;
         for(auto nxt : bus[cur.second]){
-            if(cur.first + nxt.second <= dist[nxt.first]){
+            if(!visit[nxt.first] && (cur.first + nxt.second <= dist[nxt.first])){
                 dist[nxt.first] = cur.first + nxt.second;
                 pq.push({dist[nxt.first], nxt.first});
             }
