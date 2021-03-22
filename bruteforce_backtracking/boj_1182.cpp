aca69624a8e512a495ab3ca95a1bc1cc1931f3f9
yellowsummer <cshee9812@gmail.com>
2021-03-22 22:15:27 +0900

    bruteforce 1182번 정답

--- bruteforce_backtracking/boj_1182.cpp
+++ bruteforce_backtracking/boj_1182.cpp
@@ -2,27 +2,34 @@
 using namespace std;
 
 int n, s;
-int a[20];
+int a[21];
 int ans;
+
+void solve(int tmp, int i){
+    if(i > n) return;
+
+    if(tmp + a[i] == s){
+        ans++;
+    }
+    for(int j = i + 1; j <= n; j++){
+        solve(tmp + a[i], j);
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
 
     cin>> n>> s;
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i <= n; i++){
         cin>> a[i];
     }
 
-    for(int i = 0; i < n; i++){// 수열의 원소 개수 -1
-        for(int j = 0; j + i < n; j++){// 수열의 시작하는 수
-            int tmp = 0;
-            for(int k = 0; k <= i; k++){
-                tmp += a[j + k];
-            }
-            if(tmp == s) {
-                ans++;
-            }
-        }
+    
+    for(int i = 1; i <= n; i++){
+        int tmp = 0;
+        solve(tmp, i);
     }
+    
     cout<< ans;
 }
